4f6011bcda7e8cf10f3bd1a3106ef70866293668
Lucas barros <fallenagamesytb@gmail.com>
2024-04-09 22:59:14 -0300

    fix: fix contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,7 @@
 import { useDispatch } from 'react-redux'
 
 import * as S from './styles'
-import { register, remove } from '../../store/reducers/contacts'
+import { remove } from '../../store/reducers/contacts'
 import ContactCLass from '../../models/Contact'
 import { Buttons } from '../../styles'
 
@@ -16,7 +16,6 @@ const Contact = ({ name, local, email, phone, id }: Props) => {
       <S.EmailNumber>{phone}</S.EmailNumber>
       <S.EmailNumber>{email}</S.EmailNumber>
       <S.Local>{local}</S.Local>
-      <Buttons onClick={() => dispatch(register(id))}>Adicionar</Buttons>
       <Buttons onClick={() => dispatch(remove(id))}>Remover</Buttons>
     </S.ContactCd>
   )
